eddcbc999e1d47736506b59e48ae38dc2905aa3e
Jeremy Moreau <jeremyfs.moreau@gmail.com>
2018-02-16 11:34:31 +0100

    debut integration

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
@@ -9,6 +10,8 @@ var User = require('./schema/user');
 //Routes
 var userRoutes = require('./routes/users');
 var pokemonRoutes = require('./routes/pokemons');
+var index = require('./routes/index');
+
 
 var config = require('./config.js');
 
@@ -20,6 +23,11 @@ var port = process.env.PORT || 3000;
 app.set('superSecret', config.secret);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/', index);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 
 userRoutes(app);
 pokemonRoutes(app);
